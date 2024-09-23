9fd930ec52cf16b96f5c3cfbe6f9ea625d96fdd9
samira ferreira <samiraferreiramg03@gmail.com>
2024-09-22 21:34:34 -0300

    refactor: ♻ refactoring the axios request

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,32 +1,37 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import './header.css'
-import logo from '../../image/pet.png'
+import './header.css';
+import logo from '../../image/pet.png';
 
 function Header() {
   const [ong, setOng] = useState({});
-  const apiPrefix = 'http://localhost:4000'; 
+  const apiPrefix = 'http://localhost:4000';
+
+  
+  const fetchOngInfo = async () => {
+    try {
+      const response = await axios.get(`${apiPrefix}/ong`);
+      setOng(response.data.ongInfo);
+    } catch (error) {
+      console.error("Erro ao buscar dados da ONG:", error);
+    }
+  };
 
   useEffect(() => {
-    axios.get(`${apiPrefix}/ong`) 
-      .then(response => {
-        setOng(response.data.ongInfo); 
-      })
-      .catch(error => console.error("Erro ao buscar dados da ONG:", error));
-  }, [apiPrefix]);
+    fetchOngInfo();
+  }, [apiPrefix]); 
 
-
- return (
+  return (
     <header className="header">
       <div className="container">
-      <div className="logo-name"> 
-  <img src={logo} alt="Logo da ONG" />
-  <h2>{ong.name}</h2> 
-</div>
+        <div className="logo-name">
+          <img src={logo} alt="Logo da ONG" />
+          <h2>{ong.name}</h2> 
+        </div>
         <nav className="menu">
           <a href="/">Início</a>
           <a href="/adocao">Adoção</a>
-          <a href="/Sobre nós">sobre nós</a>
+          <a href="/Sobre nós">Sobre nós</a>
         </nav>
       </div>
     </header>
