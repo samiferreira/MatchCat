720356484ff2bc2522dc39ed01a4593786e609b9
samira ferreira <samiraferreiramg03@gmail.com>
2024-09-22 21:34:16 -0300

    refactor: ♻ refactoring the axios request

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -2,27 +2,32 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import './footer.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {  faInstagram,  faLinkedin } from '@fortawesome/free-brands-svg-icons'; 
-
+import { faInstagram, faLinkedin } from '@fortawesome/free-brands-svg-icons'; 
 
 function Footer() {
   const [ong, setOng] = useState({});
-  const apiPrefix = 'http://localhost:4000'; 
+  const apiPrefix = 'http://localhost:4000';
+
+
+  const fetchOngInfo = async () => {
+    try {
+      const response = await axios.get(`${apiPrefix}/ong`);
+      setOng(response.data.ongInfo);
+    } catch (error) {
+      console.error("Erro ao buscar dados da ONG:", error);
+    }
+  };
 
   useEffect(() => {
-    axios.get(`${apiPrefix}/ong`) 
-      .then(response => {
-        setOng(response.data.ongInfo); 
-      })
-      .catch(error => console.error("Erro ao buscar dados da ONG:", error));
-  }, [apiPrefix]);
+    fetchOngInfo();
+  }, [apiPrefix]); 
 
   return (
     <footer className="footer">
       <div className="container">
         <div className="ong-info">
           <h2>{ong.name}</h2>
-          <p>Endereço: {ong.adress} </p> 
+          <p>Endereço: {ong.adress}</p> 
           <p>Telefone: {ong.phone}</p>
           <p>Email: {ong.email}</p>
         </div>
